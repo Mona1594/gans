76d9660bce21df27c1c881c6c72ca4facee66543
prashantjoshi <58854114+joshiprashanthd@users.noreply.github.com>
2020-07-03 11:36:47 +0530

    Change generator and discriminator architecture

--- tensorflow-models/wassertein_gan.py
+++ tensorflow-models/wassertein_gan.py
@@ -1,42 +1,44 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist, fashion_mnist
 
 
 class config:
     IMG_HEIGHT = 28
     IMG_WIDTH = 28
     CHANNELS = 1
-    EPOCHS = 10000
+    EPOCHS = 25000
     CLIP = 0.01
     CRITIC_SIZE = 5
     BATCH_SIZE = 64
     LATENT_DIM = 100
-    LEARNING_RATE = 0.00001
-    LAMBDA = 10
-    BETA_1 = 0
-    BETA_2 = 0.9
-
-    LOG_INTERVAL = 1000
+    LEARNING_RATE = 0.00005
+    LOG_INTERVAL = 500
     SAMPLE_INTERVAL = 1000
 
+class ClipConstraint(tf.keras.constraints.Constraint):
+    def __init__(self, clip_value):
+      self.clip_value = clip_value
+
+    def __call__(self, weights):
+      return tf.clip_by_value(weights, -self.clip_value, self.clip_value)
 
 class WasserteinGAN:
     def __init__(self):
         self.image_shape = (config.IMG_HEIGHT, config.IMG_WIDTH, config.CHANNELS)
-        self.optimizer_ascent = tf.keras.optimizers.RMSprop(learning_rate=-config.LEARNING_RATE)
-        self.optimizer_descent = tf.keras.optimizers.RMSprop(learning_rate=config.LEARNING_RATE)
+        self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=config.LEARNING_RATE)
         self.kernel_init = tf.keras.initializers.RandomNormal(stddev=0.02)
+        self.constraint = ClipConstraint(0.01)
         
         print("Loading Data...")
-        (self.train_images, _), (_, _) = mnist.load_data()
+        (self.train_images, _), (_, _) = fashion_mnist.load_data()
 
         print("Normalizing Data...")
         self.train_images = self.train_images / 127.5 - 1.
 
         self.train_images = np.expand_dims(self.train_images, axis=3)
-        print("Data Shape : ", self.train_images.shape)
+        print("Train Data Shape : ", self.train_images.shape)
         print()
 
         print("Building Generator...")
@@ -52,30 +54,29 @@ class WasserteinGAN:
         model = tf.keras.Sequential([
             tf.keras.layers.Dense(7 * 7 * 256, input_dim=config.LATENT_DIM),
             tf.keras.layers.Reshape((7, 7, 256)),
-            tf.keras.layers.Conv2DTranspose(128, 3, strides=1, padding='SAME', kernel_initializer=self.kernel_init),
+            tf.keras.layers.Conv2DTranspose(128, 4, strides=1, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Conv2DTranspose(64, 3, strides=2, padding='SAME', kernel_initializer=self.kernel_init),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.Conv2DTranspose(64, 4, strides=2, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Conv2DTranspose(1, 3, strides=2, padding='SAME', kernel_initializer=self.kernel_init),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.Conv2DTranspose(1, 4, strides=2, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init),
             tf.keras.layers.Activation('tanh')
         ])
         return model
 
     def build_discriminator(self):
         model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, 3, strides=2, input_shape=self.image_shape, padding='SAME', kernel_initializer=self.kernel_init),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Conv2D(64, 3, strides=2, padding='SAME', kernel_initializer=self.kernel_init),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Dropout(0.1),
-            tf.keras.layers.Conv2D(128, 3, strides=2, padding='SAME', kernel_initializer=self.kernel_init),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Conv2D(32, 3, strides=2, input_shape=self.image_shape, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init, kernel_constraint=self.constraint),
+            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.Conv2D(64, 3, strides=2, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init, kernel_constraint=self.constraint),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.Conv2D(128, 3, strides=2, padding='SAME', use_bias=False, kernel_initializer=self.kernel_init, kernel_constraint=self.constraint),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1),
-            tf.keras.layers.Activation('sigmoid')
         ])
         return model
     
@@ -88,26 +89,26 @@ class WasserteinGAN:
 
         g_loss = -loss
         gradients =  tape.gradient(loss, self.generator.trainable_variables)
-        self.optimizer_descent.apply_gradients(zip(gradients, self.generator.trainable_variables))
+        self.optimizer.apply_gradients(zip(gradients, self.generator.trainable_variables))
         return g_loss
-    
+
     def train_discriminator_step(self):
         d_loss = []
-        
+
         for i in range(config.CRITIC_SIZE):
             real_images = tf.convert_to_tensor(self.random_images())
             noise = tf.random.normal((config.BATCH_SIZE, config.LATENT_DIM))
-            fake_images = self.generator(noise, training=False)
 
             with tf.GradientTape() as tape:
+                fake_images = self.generator(noise, training=True)
                 real_pred = self.discriminator(real_images, training=True)
                 fake_pred = self.discriminator(fake_images, training=True)
-                loss = tf.reduce_mean(real_pred) - tf.reduce_mean(fake_pred)            
+                loss = -(tf.reduce_mean(real_pred) - tf.reduce_mean(fake_pred))            
 
             d_loss.append(loss)
 
             gradients = tape.gradient(loss, self.discriminator.trainable_variables)
-            self.optimizer_ascent.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
+            self.optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
         
         d_loss = tf.reduce_mean(d_loss)
         return d_loss
@@ -138,7 +139,7 @@ class WasserteinGAN:
         self.generate_progress_graph()
 
     def log_progress(self, epoch, g_loss, d_loss, accuracy=None):
-        print("Epoch {}/{} :".format(epoch+1, config.EPOCHS))
+        print("Epoch {}/{} :".format(epoch, config.EPOCHS))
         print("    [G Loss - {:.4f}]\t[D Loss - {:.4f}".format(g_loss, d_loss), end='')
         if accuracy is not None:
             print(" | D Acc - {:.4f}]".format(accuracy))
@@ -188,8 +189,3 @@ class WasserteinGAN:
         plt.savefig(
             "/content/image_at_{:04d}.png".format(epoch), bbox_inches='tight')
         plt.close(fig)
-
-
-if __name__ == "__main__":
-    wgan = WasserteinGAN()
-    wgan.train()
